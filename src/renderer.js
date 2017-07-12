18b8c0c4dcc649826b91448a1a38300ea1c075f4
singhsidhukuldeep <singhsidhukuldeep@gmail.com>
2017-07-13 02:28:01 +0530

    Changes to functionality

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,8 @@ import loader from 'monaco-loader';
 import { remote } from 'electron';
 import FileManager from './filemanager';
 
+const fs = require('fs');
+
 loader().then((monaco) => {
   const editor = monaco.editor.create(document.getElementById('container'), {
     language: 'javascript',
@@ -9,7 +11,17 @@ loader().then((monaco) => {
     automaticLayout: true,
   });
 
+
   const fileManager = new FileManager({ editor, monaco });
+
+
+  ipcRenderer.on('open-file', (e, url) => {
+      console.log(url);
+      fs.readFile(url.slice(7), 'utf-8', (err, data)=> {
+        editor.setModel(monaco.editor.createModel(data,'javascript'))
+      })
+  });
+
 
   remote.getCurrentWindow().show();
 });
