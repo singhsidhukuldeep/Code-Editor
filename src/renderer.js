f867c45edeff3ab4cbdf8c896a0cf47a9ac66f28
singhsidhukuldeep <singhsidhukuldeep@gmail.com>
2017-07-13 22:15:56 +0530

    code cleaning

--- src/renderer.js
+++ src/renderer.js
@@ -2,8 +2,6 @@ import loader from 'monaco-loader';
 import { remote } from 'electron';
 import FileManager from './filemanager';
 
-const fs = require('fs');
-
 loader().then((monaco) => {
   const editor = monaco.editor.create(document.getElementById('container'), {
     language: 'javascript',
@@ -11,17 +9,7 @@ loader().then((monaco) => {
     automaticLayout: true,
   });
 
-
   const fileManager = new FileManager({ editor, monaco });
-
-
-  ipcRenderer.on('open-file', (e, url) => {
-      console.log(url);
-      fs.readFile(url.slice(7), 'utf-8', (err, data)=> {
-        editor.setModel(monaco.editor.createModel(data,'javascript'))
-      })
-  });
-
 
   remote.getCurrentWindow().show();
 });
